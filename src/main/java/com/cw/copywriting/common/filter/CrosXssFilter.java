cc1e3906382dd9230ccd5edb39df525135bf607f
liaoziyang <liaoziyang@nursingworld.com.cn>
2020-05-12 16:52:36 +0800

    filter

---
+++ src/main/java/com/cw/copywriting/common/filter/CrosXssFilter.java
@@ -0,0 +1,10 @@
+package com.cw.copywriting.common.filter;
+
+/**
+ * @author LiaoZiYang
+ * @version 1.0
+ * @date 2020/5/11 9:53
+ * @Desc
+ */
+public class CrosXssFilter {
+}
---
+++ src/main/java/com/cw/copywriting/common/filter/XssFilter.java
@@ -0,0 +1,10 @@
+package com.cw.copywriting.common.filter;
+
+/**
+ * @author LiaoZiYang
+ * @version 1.0
+ * @date 2020/5/11 10:22
+ * @Desc
+ */
+public class XssFilter {
+}
